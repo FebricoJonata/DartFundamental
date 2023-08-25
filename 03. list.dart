7fe60d151f0208608ef393c564f6a1ddb14fe5a9
FebricoJ <riko967gmail.com>
2023-08-25 18:12:17 +0700

    update project 1.1

--- 03. list.dart
+++ 03. list.dart
@@ -1,1 +1,28 @@
-void main() {}
+void main() {
+  List<int> listOfInt = [17, 4, 7, 10];
+
+  listOfInt.sort();
+  print("Show list using for loop");
+  for (int i = 0; i < listOfInt.length; i++) {
+    print(listOfInt[i]);
+  }
+
+  
+  listOfInt.add(2);
+  listOfInt.sort((a, b) => b-a);
+  print("\nShow list using For In");
+  for (int number in listOfInt) {
+    print(number);
+  }
+
+  listOfInt.remove(2);
+  listOfInt.removeAt(0);
+  //Remove all element
+  listOfInt.removeRange(0, listOfInt.length);
+  
+  print("\nShow List using for each");
+  listOfInt.forEach((element) {
+    print(element);
+  });
+
+}
--- 04. function.dart
+++ 04. function.dart
@@ -1,1 +1,21 @@
-void main() {}
+//Optional Parameter
+String greet(String sender, String message, {String? to}){
+  return "Message From " + sender + " " + message +
+      " " + ((to != null) ? to : "");
+}
+
+//Arrow Syntax
+int add(int a, int b) => a+b;
+
+void main() {
+
+  //Anonymous function
+  (int a, int b){
+    a+b;
+  };
+
+  print(greet("Rico", "Horaay", to: "Hoho"));
+
+  print(add(2, 5));
+
+}
